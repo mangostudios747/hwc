8328a0923e46c68c5f43322a716fddfb4b78b6ae
saumyasinghal747 <saumyasmathtutoring@gmail.com>
2023-05-02 21:49:14 -0700

    account deletion

--- pages/app/settings.vue
+++ pages/app/settings.vue
@@ -2,7 +2,7 @@
   <div>
     <h2>Settings</h2>
     <button @click="logout">logout</button>
-    <button style="color:red;" >delete account</button>
+    <button @click="deleteAccount" style="color:red;" >delete account</button>
   </div>
 </template>
 
@@ -12,6 +12,29 @@ const router = useRouter()
 function logout() {
   onLogout();
   router.push("/");
+}
+
+async function deleteAccount(){
+  if (!confirm("are you sure you want to delete your account?")) return;
+  const pwd = prompt("please enter your password to delete your account.");
+  const deleteMutation = gql`
+    mutation deleteAccount($password: String!) {
+      deleteAccount(password: $password) {
+        success
+        error
+      }
+    }
+  `
+  const { mutate } = useMutation(deleteMutation, { variables: { password: pwd } });
+  const {
+    data: {
+      deleteAccount: { success, error },
+    },
+  } = await mutate();
+  if (success){
+    return logout()
+  }
+  alert("account deletion not successful. your password was incorrect.")
 }
 
 </script>
